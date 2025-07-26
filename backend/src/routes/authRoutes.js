152817d90047168c3bcb07e1e829aca74b2cecc3
Raseeca Kashelkar <raseecakashelkar163@gmail.com>
2025-07-26 13:18:01 +0530

    User-Logout-API

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,12 +3,14 @@ const {
   register,
   login,
   getCurrentUser,
+  logout,
 } = require("../controllers/authController");
-const {authMiddleware} = require("../middleware/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", authMiddleware, getCurrentUser);
+router.post("/logout", authMiddleware, logout);
 
 module.exports = router;
