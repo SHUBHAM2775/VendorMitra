aa6cae1635e1be085b8a6c3b718f3fb7545b940f
Shreya Shukla <shreyashukla20042005@gmail.com>
2025-07-26 13:09:45 +0530

    Verification-Pending-Count

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { verifySupplier, getPendingVerifications } = require("../controllers/adminController");
+const { verifySupplier, getPendingVerifications, getPendingVerificationCount } = require("../controllers/adminController");
 const {authMiddleware, isAdmin} = require("../middleware/authMiddleware");
 
 router.get("/pending-verifications", authMiddleware,isAdmin, getPendingVerifications);
+router.get("/verification-pending-count", authMiddleware, isAdmin, getPendingVerificationCount);
 router.put("/verify-supplier/:id", authMiddleware ,isAdmin, verifySupplier);
 
 module.exports = router;
