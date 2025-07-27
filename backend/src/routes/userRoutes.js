1c5cc53e4125068d91dc9f42181ec7f3eaa0ea75
Raseeca Kashelkar <raseecakashelkar163@gmail.com>
2025-07-27 16:15:04 +0530

    nothing-imp

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,17 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getUserProfile, updateUserProfile, uploadKycDocs, getVerifiedSuppliers } = require('../controllers/userController');
-const {authMiddleware} = require("../middleware/authMiddleware");
-const upload = require('../utils/upload');
+const {
+  getUserProfile,
+  updateUserProfile,
+  uploadKycDocs,
+  getVerifiedSuppliers,
+} = require("../controllers/userController");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const upload = require("../utils/upload");
 
 // Route: GET /users/:id
-router.get('/get-user/:id', authMiddleware, getUserProfile);
+router.get("/get-user/:id", authMiddleware, getUserProfile);
 
 // Route: PUT /users/:id
-router.put('/update-user/:id', authMiddleware, updateUserProfile);
+router.put("/update-user/:id", authMiddleware, updateUserProfile);
 
-router.post('/:id/verify', authMiddleware, upload.array('kycDocs', 5), uploadKycDocs);
+router.post(
+  "/:id/verify",
+  authMiddleware,
+  upload.array("kycDocs", 5),
+  uploadKycDocs
+);
 
-router.get('/suppliers', authMiddleware, getVerifiedSuppliers);
+router.get("/suppliers", authMiddleware, getVerifiedSuppliers);
 
 module.exports = router;
